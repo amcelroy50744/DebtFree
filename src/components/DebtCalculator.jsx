ea0a611c0c7acd82dbcc5f15d8a51e1f8a5a84a9
Andre McElroy <amcelroy50744@gmail.com>
2023-02-27 14:45:20 -0600

    Allow minimum payment Change comparison from strings to numbers Handle Max payment

--- src/components/DebtCalculator.jsx
+++ src/components/DebtCalculator.jsx
@@ -1,50 +1,49 @@
 import React from "react";
 
 class DebtCalculator extends React.Component {
-    constructor(debt,interest, loanPayments, payment) {
-        super();
-        this.loan = debt; 
-        this.interest = interest;
-        this.loanPayments = [];
-        this.payment = payment;
-        
+  constructor(debt, interest, loanPayments, payment) {
+    super();
+    this.loan = debt;
+    this.interest = interest;
+    this.loanPayments = [];
+    this.payment = payment;
+  }
+  findInterest = (debt, interest) => (interest / 1200) * debt;
+
+  findPrincipalPayment = (debt) => debt * 0.01;
+
+  minimumPayment = (debt, interest) => {
+    if (debt <= 100) {
+      return debt;
+    } else {
+      return (this.findInterest(debt, interest) + debt * 0.01).toFixed(2);
     }
-    findInterest = (debt, interest) => 
-        (interest/1200) * debt; 
+  };
 
-    findPrincipalPayment = (debt) => (debt * .01);
-    
-     minimumPayment = (debt, interest) => {
-        if (debt <= 100){
-            return debt
-        }else{
-         return (this.findInterest(debt,interest) + debt * .01).toFixed(2);
-        }
+  IsGreaterThanMinimum = (payment, minimumPayment) => {
+    if (payment >= minimumPayment) {
+      return false;
+    } else return true;
+  };
+
+  findBalance = (debt, interest, minimumPayment, payment) => {
+    let balance = Number(debt);
+    let interestPayment = this.findInterest(debt, interest);
+    if (Number(payment) >= minimumPayment && balance > 100) {
+      let x = payment - interestPayment;
+      balance = (balance - x).toFixed(2);
+      if (balance > 0) {
+        return balance;
+      }
+      else { return "Debt Free"}
     }
-
-    IsGreaterThanMinimum = (payment, minimumPayment) => {
-        if (payment >= minimumPayment){
-            return false;
-        }else return true;
+    if (balance < 100) {
+      return "DebtFree";
     }
-    
-    findBalance = (debt,interest, minimumPayment, payment) => {
-        let balance = debt;
-        let interestPayment = this.findInterest(debt,interest)
-        if(payment >= minimumPayment && balance > 100 ) {
-                let x = payment - interestPayment
-                 balance = (balance - x).toFixed(2);
-                 return balance;
-        }
-        if (balance < 100) {
-            return 'DebtFree'
-        }
-        
-        }
-        
+    if (payment === balance) {
+      return "DebtFree";
     }
-
-
-
+  };
+}
 
 export default DebtCalculator;
--- src/components/DebtForm.jsx
+++ src/components/DebtForm.jsx
@@ -6,62 +6,74 @@ import PaymentList from "./paymentList";
 function DebtForm() {
   const [inputArray, setInputArray] = useState([]);
   const [details, setDetails] = useState({
-    loan: 0,
-    interest: 0,
-    payment: '',
+    loan: "",
+    interest: "",
+    payment: "",
     balance: "",
     id: "",
     paymentsLeft: "",
-    lastPayment: ''
+    lastPayment: "",
   });
-  
 
-  let { loan, interest, payment, balance, id, paymentsLeft, lastPayment } = details;
+  let { loan, interest, payment, balance, id, paymentsLeft, lastPayment } =
+    details;
   function submitPayment(e) {
     e.preventDefault();
-   if(details.payment>= minimumPayment){
-    if(balance >= 100) {
-      setInputArray([
-      ...inputArray,
-      { loan, interest, payment, balance, id: uuid(), paymentsLeft, lastPayment },
-    ]);
-    setDetails({
-      loan: remainingBalance,
-      interest: interest,
-      payment: payment,
-      balance: balance,
-      id: "",
-      payments: "",
-      lastPayment: balance
-    });
-  }else {
-    setInputArray([
-      ...inputArray,
-      { loan, interest, balance, lastPayment, id: uuid(), paymentsLeft },
-    ]);
-    setDetails({
-      loan: remainingBalance,
-      interest: interest,
-      payment: payment,
-      balance: balance,
-      id: "",
-      payments: "",
-      paymentsLeft: '',
-      lastPayment: balance
-    });
-  }
-}
+    if (
+      Number(details.payment) > Number(minimumPayment) ||
+      Number(details.payment) === Number(minimumPayment)
+    ) {
+      if (Number(balance) >= 100) {
+        setInputArray([
+          ...inputArray,
+          {
+            loan,
+            interest,
+            payment,
+            balance,
+            id: uuid(),
+            paymentsLeft,
+            lastPayment,
+          },
+        ]);
+        setDetails({
+          loan: remainingBalance,
+          interest: interest,
+          payment: payment,
+          balance: balance,
+          id: "",
+          payments: "",
+          lastPayment: balance,
+        });
+      } else {
+        setInputArray([
+          ...inputArray,
+          { loan, interest, balance, lastPayment, id: uuid(), paymentsLeft },
+        ]);
+        setDetails({
+          loan: remainingBalance,
+          interest: interest,
+          payment: payment,
+          balance: balance,
+          id: "",
+          payments: "",
+          paymentsLeft: "",
+          lastPayment: balance,
+        });
+      }
+    }
   }
 
   let userBalance = new DebtCalculator();
   userBalance.loan = details.loan;
   balance = userBalance;
   userBalance.interest = details.interest;
+  let userInterest = userBalance.findInterest(balance,userBalance.interest)
   let minimumPayment = userBalance.minimumPayment(
     userBalance.loan,
     userBalance.interest
   );
-  
+
   let remainingBalance = userBalance.findBalance(
     userBalance.loan,
     userBalance.interest,
@@ -69,9 +81,10 @@ function DebtForm() {
     details.payment,
     userBalance.loanPayments
   );
-  balance = remainingBalance;
+  let newInterest = userBalance.findInterest(remainingBalance,userBalance.interest)
+  balance = remainingBalance
   paymentsLeft = (remainingBalance / details.payment).toFixed(0);
-  
+
   const handleChange = (e) => {
     e.preventDefault();
     const { name, value } = e.target;
@@ -95,23 +108,20 @@ function DebtForm() {
         <input
           type="number"
           name="interest"
+          inputMode="decimal"
           onChange={handleChange}
           autoComplete="off"
           placeholder="Enter Loan Interest"
         />
         <h3>
           Minimum Payment is: <span>${minimumPayment}</span>
-          
         </h3>
-        <h3>Payments Left : <span>{paymentsLeft}</span></h3>
+        <h3>
+          Payments Left : <span>{paymentsLeft}</span>
+        </h3>
         <h3>Enter Payment Amount</h3>
-        <input
-         
-          type="number"
-          name="payment"
-          onChange={handleChange}
-        />
-        <button onClick={submitPayment} className="btn" >
+        <input type="number" name="payment" inputMode="decimal" onChange={handleChange} />
+        <button onClick={submitPayment} className="btn">
           Submit Payment
         </button>
         <h3>Record of Payments</h3>
